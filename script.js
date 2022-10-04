de899b1fe9dd07ac0dc64983cee6a9cb7fd9f667
BrunoSouza <desouza88@hotmail.com>
2022-10-04 15:38:46 -0300

    teoricamente terminei o requisito dois

--- script.js
+++ script.js
@@ -8,4 +8,28 @@ elementMain.appendChild(elementH1);
 
 let elementSection = document.createElement('section');
 elementSection.id = 'color-palette';
+elementSection.style.display = 'flex';
 elementMain.appendChild(elementSection);
+
+for (let index = 0; index < 4; index +=1){
+let colorOptions = document.createElement('div');
+
+colorOptions.style.height = '80px';
+colorOptions.style.width = '80px';
+colorOptions.style.border = '1px solid black';
+colorOptions.style.borderRadius = '75%';
+colorOptions.style.margin = '10px';
+colorOptions.className = 'color';
+colorOptions.id = 'color-palette';
+elementSection.appendChild(colorOptions);
+}
+
+let colorOne = document.getElementsByClassName('color')[0];
+let colorTwo = document.getElementsByClassName('color')[1];
+let colorThree = document.getElementsByClassName('color')[2];
+let colorFour = document.getElementsByClassName('color')[3];
+
+colorOne.style.backgroundColor = 'black';
+colorTwo.style.backgroundColor = 'blue';
+colorThree.style.backgroundColor = 'green';
+colorFour.style.backgroundColor = 'red';
