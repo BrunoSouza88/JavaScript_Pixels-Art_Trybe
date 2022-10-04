3a6f05beb26665ee6545826da2295fda17449916
BrunoSouza <desouza88@hotmail.com>
2022-10-04 15:14:39 -0300

    Criação de um elemento Main para servir de pai de h1 e uma section que irá ser o requisisto 2

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let elementMain = document.createElement('main');
+document.body.appendChild(elementMain);
+
 let elementH1 = document.createElement('h1');
 elementH1.id = 'title';
 elementH1.innerText = 'Paleta de Cores';
+elementMain.appendChild(elementH1);
+
+let elementSection = document.createElement('section');
+elementSection.id = 'color-palette';
+elementMain.appendChild(elementSection);
