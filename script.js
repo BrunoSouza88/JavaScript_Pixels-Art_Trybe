de10add46c74be88627b2a218e097f4f08eb871d
BrunoSouza <desouza88@hotmail.com>
2022-10-09 22:23:50 -0300

    Ajustes de lint no JS

--- script.js
+++ script.js
@@ -1,58 +1,55 @@
-let color1 = document.getElementsByClassName('color')[0];
-let color2 = document.getElementsByClassName('color')[1];
-let color3 = document.getElementsByClassName('color')[2];
-let color4 = document.getElementsByClassName('color')[3];
-let buttonChangeColor = document.getElementById('button-random-color');
+const color1 = document.getElementsByClassName('color')[0];
+const color2 = document.getElementsByClassName('color')[1];
+const color3 = document.getElementsByClassName('color')[2];
+const color4 = document.getElementsByClassName('color')[3];
 
 function colorBlack() {
-    color1.style.backgroundColor = 'black';
+  color1.style.backgroundColor = 'black';
 }
 
 function randomColor() {
-    let r = Math.random() * 256;
-    let g = Math.random() * 256;
-    let b = Math.random() * 256;
+  const r = Math.random() * 256;
+  const g = Math.random() * 256;
+  const b = Math.random() * 256;
 
-    return `rgb(${r}, ${g}, ${b})`;
-};
-
+  return `rgb(${r}, ${g}, ${b})`;
+}
 
 function otherColors() {
-    color2.style.backgroundColor = randomColor();
-    color3.style.backgroundColor = randomColor();
-    color4.style.backgroundColor = randomColor();
+  color2.style.backgroundColor = randomColor();
+  color3.style.backgroundColor = randomColor();
+  color4.style.backgroundColor = randomColor();
 }
 
 function creatTable() {
-    let elementSectionTable = document.createElement('table');
-    elementSectionTable.id = 'pixel-board';
+  const elementSectionTable = document.createElement('table');
+  elementSectionTable.id = 'pixel-board';
 
-    document.getElementsByTagName('main')[0].appendChild(elementSectionTable);
+  document.getElementsByTagName('main')[0].appendChild(elementSectionTable);
 
-    for (let index = 0; index <= 4; index += 1) {
-        let tableTr = document.createElement('tr');
-        elementSectionTable.appendChild(tableTr);
+  for (let index = 0; index <= 4; index += 1) {
+    const tableTr = document.createElement('tr');
+    elementSectionTable.appendChild(tableTr);
 
-        for (let index = 0; index <= 4; index += 1) {
-            let tableTd = document.createElement('td');
+    for (let index1 = 0; index1 <= 4; index1 += 1) {
+      const tableTd = document.createElement('td');
 
-            tableTd.className = 'pixel';
-            tableTr.appendChild(tableTd);
-
-        };
-    };
-};
+      tableTd.className = 'pixel';
+      tableTr.appendChild(tableTd);
+    }
+  }
+}
 
 function selectColor() {
-    let colorSelected = document.getElementsByClassName('color');
-    colorSelected.className = ' selected';
-};
+  const colorSelected = document.getElementsByClassName('color');
+  colorSelected.className = ' selected';
+}
 
 function addClassSelected(event) {
-    let elementSelcted = document.getElementsByClassName('selected')[0];
-    elementSelcted.classList.remove('selected');
-    event.target.classList.add('selected');
-};
+  const elementSelcted = document.getElementsByClassName('selected')[0];
+  elementSelcted.classList.remove('selected');
+  event.target.classList.add('selected');
+}
 
 color1.addEventListener('click', addClassSelected);
 color2.addEventListener('click', addClassSelected);
@@ -60,34 +57,30 @@ color3.addEventListener('click', addClassSelected);
 color4.addEventListener('click', addClassSelected);
 
 function holdColor() {
-    let setNewColor = document.querySelector('.selected').style.backgroundColor;
-    return setNewColor;
-};
+  const setNewColor = document.querySelector('.selected').style.backgroundColor;
+  return setNewColor;
+}
 
 function coloring() {
-    let squarePixels = document.getElementsByClassName('pixel');
-    for (let index = 0; index < squarePixels.length; index += 1) {
-        squarePixels[index].addEventListener('click', function () {
-            squarePixels[index].style.backgroundColor = holdColor();
-        });
-    };
-
-};
+  const squarePixels = document.getElementsByClassName('pixel');
+  for (let index = 0; index < squarePixels.length; index += 1) {
+    squarePixels[index].addEventListener('click', () => {
+      squarePixels[index].style.backgroundColor = holdColor();
+    });
+  }
+}
 
 function clearBoard() {
-    let clearBoard = document.getElementsByClassName('pixel');
-    for (let index = 0; index < clearBoard.length; index += 1) {
-        clearBoard[index].style.backgroundColor = 'white'
+  const clearBoard1 = document.getElementsByClassName('pixel');
+  for (let index = 0; index < clearBoard1.length; index += 1) {
+    clearBoard1[index].style.backgroundColor = 'white';
+  }
+}
 
-    };
-};
-
-
-// Chamando funções                                                                     
+// Chamando funções
 colorBlack();
 otherColors();
 creatTable();
 selectColor();
 coloring();
-
-// Oi Renan!
+clearBoard();
